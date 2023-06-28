2a7c49eb6eb4c13f968fa8c9435939101545df41
LeonardoRinaldi92 <leonardo.rinaldi.ph@gmail.com>
2023-06-28 12:55:26 +0200

    ok invio api

--- app/Http/Controllers/Api/ProjectCrontroller.php
+++ app/Http/Controllers/Api/ProjectCrontroller.php
@@ -3,9 +3,18 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Admin\Project;
 use Illuminate\Http\Request;
 
 class ProjectCrontroller extends Controller
 {
-    //
+    public function index() {
+    
+        $projects = Project::with('type','tags')->paginate(6);
+
+        return response()->json([
+            'success' => true,
+            'posts' => $projects
+        ]);
+    }
 }
